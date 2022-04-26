277db7e44c07901f40c97babb6a3dd11aa767c71
rokas <rokasra@gmail.com>
2022-04-26 19:03:03 +0300

    empty server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const { port } = require('./config');
+
+const app = express();
+app.listen(port, () => console.log('express is online', port));
+
+app.get('/', (request, response) => {
+  response.json('Hello World');
+});
