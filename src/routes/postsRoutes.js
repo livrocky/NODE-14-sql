b6c8b7acf3f747b679517e9da31288a8e8934f6e
rokas <rokasra@gmail.com>
2022-04-28 15:20:15 +0300

    class notes

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -4,8 +4,23 @@ const { dbConfig } = require('../config');
 
 // sukurti routeri
 const postRoutes = express.Router();
-// routeri impoprtuoti i server js
+// routeri importuoti i server js
 // sukurti tuscia route ir isitikinti kad veikia
+// GET /api/first-posts - parsiusti pirmus 2 posts (LIMIT)
+postRoutes.get('/posts', async (req, res) => {
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    const sql = 'SELECT * FROM posts';
+    const [rows] = await conn.query(sql);
+    res.json(rows);
+  } catch (error) {
+    console.log('error getting all posts', error);
+    res.sendStatus(500);
+  } finally {
+    await conn?.end();
+  }
+});
 postRoutes.get('/first-posts', async (req, res) => {
   let conn;
   try {
@@ -15,21 +30,52 @@ postRoutes.get('/first-posts', async (req, res) => {
     res.json(rows);
   } catch (error) {
     console.log('error getting first posts', error);
-    res.status(500);
+    res.sendStatus(500);
   } finally {
     await conn?.end();
   }
 });
-// GET /api/first-posts - parsiusti pirmus 2 posts (LIMIT)
 
-/// COPIED (dinamimnis)///////////////////////////////
+// GET /api/posts-by-rating/  - parsiusti visus posts isrikiuotuos nuo diziausio rating vertinimo.
+postRoutes.get('/posts-by-rating/', async (req, res) => {
+  let conn;
+  try {
+    // query param
+    const { order } = req.query;
 
+    // SELECT * FROM posts ORDER BY rating DESC
+    // Vietoj DESC gali buti tik 2 reiksmes ASC | DESC
+
+    conn = await mysql.createConnection(dbConfig);
+    const safeOrder = mysql.escape(order);
+    console.log('order ===', order);
+    console.log('safeOrder ===', safeOrder);
+    // salyga ? true :  false
+    const ascOrDesc = order === 'ASC' ? 'ASC' : 'DESC';
+    const sql = `SELECT * FROM posts ORDER BY rating ${ascOrDesc}`;
+    console.log('sql ===', sql);
+    const [rows] = await conn.query(sql);
+    res.json(rows);
+  } catch (error) {
+    console.log('error posts-by-rating', error);
+    res.sendStatus(500);
+  } finally {
+    await conn?.end();
+  }
+});
+
+// perkelti /api/posts routa is server.js i postRoute.js
+
+// sukurti 404 endpointa kuris grazintu atsakyma json formatu
+
+// GET /api/posts/name/:name - parisiusti postus kuriuos parase :name
 postRoutes.get('/posts/name/:name', async (req, res) => {
   let conn;
   try {
     const { name } = req.params;
-    console.log('name===', name);
+    console.log('name ===', name);
     conn = await mysql.createConnection(dbConfig);
+    // esam neapsaugoti nuo sql injection
     // const sql = `SELECT * FROM posts WHERE author = '${name}'`;
     // const [rows] = await conn.query(sql);
 
@@ -38,29 +84,33 @@ postRoutes.get('/posts/name/:name', async (req, res) => {
     const [rows] = await conn.execute(sql, [name]);
     res.json(rows);
   } catch (error) {
-    console.log('error in getting posts by James', error);
-    res.status(500);
+    console.log('error getting all posts', error);
+    res.sendStatus(500);
   } finally {
     await conn?.end();
   }
 });
 
-// POST /api/posts - sukurti nauja posta su duomeninis kurios gavo is post route
+// POST /api/posts - sukurti nauja posta su duomenimis kuriuos gavo is post route
 postRoutes.post('/posts', async (req, res) => {
   let conn;
   try {
     const newPostObj = req.body;
-    console.log('newPostObj===', newPostObj);
+    console.log('newPostObj ===', newPostObj);
     // const title = newPostObj.title;
     // const author = newPostObj.author;
     // const body = newPostObj.body;
+    // const rating = newPostObj.rating;
+    // ===
+
     // eslint-disable-next-line object-curly-newline
     const { title, author, body, rating } = newPostObj;
+
     conn = await mysql.createConnection(dbConfig);
     const sql = `
-  INSERT INTO posts (title, author, body, rating)
-VALUES (?, ?, ?, ?)
-  `;
+    INSERT INTO posts (title, author, body, rating)
+    VALUES (?, ?, ?, ?)
+    `;
     const [insertResultObj] = await conn.execute(sql, [title, author, body, rating]);
     if (insertResultObj.affectedRows === 1) {
       res.status(201).json(insertResultObj);
@@ -68,13 +118,37 @@ VALUES (?, ?, ?, ?)
     }
     throw new Error('affected row not 1');
   } catch (error) {
-    console.log('error getting first posts', error);
-    res.status(500);
+    console.log('error creating post', error);
+    res.sendStatus(500);
   } finally {
     await conn?.end();
   }
 });
 
-module.exports = {
-  postRoutes,
-};
+// DELETE /api/posts/:postId - istrina posta kurio id === postId
+postRoutes.delete('/posts/:postId', async (req, res) => {
+  // res.json(req.params.postId);
+  let conn;
+  try {
+    const { postId } = req.params;
+    conn = await mysql.createConnection(dbConfig);
+    const sql = 'DELETE FROM posts WHERE id = ?';
+    const [deleteRezult] = await conn.execute(sql, [postId]);
+    if (deleteRezult.affectedRows !== 1) {
+      res.status(400).json({ success: false, error: `user with id ${postId}, was not found` });
+      return;
+    }
+    if (deleteRezult.affectedRows === 1) {
+      res.json('delete ok');
+      return;
+    }
+    throw new Error('sometnig wrong in deleteRezult.affectedRows');
+  } catch (error) {
+    console.log('error DELETE posts', error);
+    res.sendStatus(500);
+  } finally {
+    await conn?.end();
+  }
+});
+
+module.exports = postRoutes;
--- src/server.js
+++ src/server.js
@@ -2,41 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-// const cors = require('cors');
 const { PORT, dbConfig } = require('./config');
-const { postRoutes } = require('./routes/postsRoutes');
+const postRoutes = require('./routes/postsRoutes');
 
 const app = express();
 
-// Middleware
+// Middle ware
 app.use(morgan('dev'));
-app.use(cors('cors'));
+app.use(cors());
 // kad express galetu gauti json siustus duomenis
 app.use(express.json());
 
-// home route
-
-app.get('/api/posts', async (req, res) => {
-  let connection;
-  try {
-    // 1 prisijungti
-    connection = await mysql.createConnection(dbConfig);
-    console.log('connected');
-    // 2 atlikti veiksma
-    const sql = 'SELECT * FROM posts';
-    const [rows, fields] = await connection.query(sql);
-    res.json(rows);
-  } catch (error) {
-    // // err gaudom klaidas
-    console.log('home route error ===', error);
-    res.status(500).json('something went wrong');
-  } finally {
-    // 3 atsijungti
-    if (connection) connection.end();
-    // connection?.close();
-  }
-});
-
 app.use('/api/', postRoutes);
 
-app.listen(PORT, () => console.log('express is online', PORT));
+// api
+app.all('*', (req, res) => {
+  res.status(404).json({ error: 'Page not found' });
+});
+
+app.listen(PORT, () => console.log('server is running on port', PORT));
